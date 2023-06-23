7e0f1dba7c66c2c156cbd9e1b802e580fa7cf70f
burakTuran00 <burakturan7410@gmail.com>
2023-06-23 11:30:44 +0300

    dictionaries

--- dictionaries.py
+++ dictionaries.py
@@ -1,4 +1,15 @@
 file_counts = {"jpg":10 , "txt":14, "csv":2, "py":23}
+
 print(file_counts)
 print(file_counts["txt"])
 print("jpg" in file_counts)
+
+file_counts["cfg"] = 8
+print(file_counts)
+del file_counts["cfg"]
+
+for extension in file_counts:
+    print(extension)
+
+for ext, amount in file_counts.items():
+    print("There are {} files with the .{} extension".format(amount,ext))
